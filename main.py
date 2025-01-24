bba58577622ed56b88a901bc6a3af792d2604f26
Lauren Kamali <kamalil@wlhs.wlwv.k12.or.us>
2025-01-24 20:41:30 +0000

    final done

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 '''
 Name: Lauren Kamali
-Date: 10/28/24
+Date: 1/24/25
 Assignment: Unit 2 and 3 Project
 '''
 
-userMonth = input("Enter the name of the month: ")
-userDay = int(input("Enter the day (1-31): ")) 
-
 def get_season(userMonth, userDay):
-    if (userMonth == "March" and userDay >= 20) or userMonth in ["April", "May"] or (userMonth == "June" and userDay <= 14):
+    if (userMonth == "March" and userDay >= 20) or userMonth in ["April", "May"] or (userMonth == "June" and userDay <= 20):
         return "Spring"
-    elif (userMonth == "June" and userDay >= 15) or userMonth in ["July", "August"] or (userMonth == "September" and userDay <= 21):
+    elif (userMonth == "June" and userDay >= 21) or userMonth in ["July", "August"] or (userMonth == "September" and userDay <= 21):
         return "Summer"
     elif (userMonth == "September" and userDay >= 22) or userMonth in ["October", "November"] or (userMonth == "December" and userDay <= 20):
         return "Fall"
     else:
         return "Winter"
 
-
-print(f"{userMonth} {userDay} is in {get_season(userMonth, userDay)}")
-
-
-
-
-
-
-
+userMonth = input("Enter the name of the month: ").capitalize()  
+try:
+    userDay = int(input("Enter the day (1-31): ")) 
+    season = get_season(userMonth, userDay)
+    print(f"The season on {userMonth} {userDay} is {season}.") 
+except ValueError:
+    print("Invalid day. Please enter a number.")
